5975243cbbbc1c980db312341568423b7fd4b653
howa <howa9495@gmail.com>
2024-09-15 17:20:35 +0800

    Setup

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(CapsuleCollider))]
+public class PlayerCharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
